2c780298ceb956451d44cf3b7676f5f11f98d8c3
Luis <luisfernandoibarra100@gmail.com>
2020-05-06 04:54:56 -0500

    DevTools de luces en una escena basica en Three

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+//Importar libreria de Three.js
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r110/build/three.module.js';
 import { TrackballControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r110/examples/jsm/controls/TrackballControls.js';
 import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r110/examples/jsm/controls/OrbitControls.js';
@@ -8,27 +8,25 @@ import { PMREMGenerator } from 'https://threejsfundamentals.org/threejs/resource
 import { PMREMCubeUVPacker } from 'https://threejsfundamentals.org/threejs/resources/threejs/r110/examples/jsm/pmrem/PMREMCubeUVPacker.js';
 import { GUI } from 'https://threejsfundamentals.org/threejs/resources/threejs/r110/examples/jsm/libs/dat.gui.module.js';
 
-var defaultLayers = ["Arms2", "Back1", "BackHolder1", "Base", "Casters", "Config", "Seat", "SeatHolder"];
 
+
+
+
+//Variables
+var container, controls;
+var camera, scene, renderer;
+var hemiLight,directional,ambient,spotLight;
 var artifactCanvas = document.getElementById('artifactCanvas');
 
-
-var container, stats, controls;
-var camera, scene, renderer, light;
-var clock = new THREE.Clock();
-var mixer;
-var bulbLight, bulbMat, hemiLight, stats;
-var meshes;
-
+//Parametros del DEvTools
 var params = {
     shadows: true,
-    exposure: 0.85,
-    bulbPower: 6,
-    hemiIrradiance: 18,
-    metalness: 0.86,
-    roughness: 1,
+    exposure: 0.7,
+    hemiIrradiance: 2.1,
+    directional: 1,
+    ambient:1,
+    spotLight: 5,
 };
-
 
 
 init();
@@ -37,47 +35,75 @@ function init() {
 
   container = document.createElement('div');
   document.body.appendChild(container);
-  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 20000);
-  camera.position.set(100, 100, 300);
 
+  //Camara
+  camera = new THREE.PerspectiveCamera( 50, innerWidth / innerHeight, 1, 10000 );
+  camera.position.set(-230, 200, 400);
+
+  //Escena
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xffffff);
 
+  //Luces
 
-  var bulbGeometry = new THREE.SphereBufferGeometry(0.02, 16, 8);
-  bulbLight = new THREE.PointLight(0xffffff, 5000, 5000, 2);
-  bulbMat = new THREE.MeshStandardMaterial({
-      emissive: 0xffffee,
-      emissiveIntensity: 1,
-      color: 0x000000
-  });
-  bulbLight.add(new THREE.Mesh(bulbGeometry, bulbMat));
-  bulbLight.position.set(0, 50, 0);
-  bulbLight.castShadow = true;
-  scene.add(bulbLight);
+   //==============================================================================================
+  //================================LUZ DIRECCIONAL===============================================
+ //==============================================================================================
 
-  hemiLight = new THREE.HemisphereLight(0xEEEEEE, 0xaaaaaa, 0.02); //0x0f0e0d
+   /* Una luz que se emite en una dirección específica.
+      Esta luz se comportará como si estuviera infinitamente lejos y los rayos producidos por ella sean todos paralelos.
+      Puede contar con sombras,  DirectionalLightShadow  
+      Cara frontal light.position.set(0,0,1);
+      Cara frontal y derecha  light.position.set(1,0,1);
+      Cara frontal,derecha y arriba  light.position.set(1,1,1); */
+
+    directional = new THREE.DirectionalLight( 0xffffff, 0.4); // Color e intensidad
+    directional.position.set(1,1,1);
+    scene.add(directional);
+
+   //==============================================================================================
+  //==============================LUZ HEMISFERICA=================================================
+ //==============================================================================================
+
+   /* Una fuente de luz colocada directamente sobre la escena, con un color que se desvanece 
+   del color del cielo al color del suelo.
+   Esta luz no se puede usar para proyectar sombras. */
+
+  hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 1); //0x0f0e0d
   scene.add(hemiLight);
 
-  var spotLight = new THREE.SpotLight( 0xffffff );
-spotLight.position.set( 10, 10, 10 );
-scene.add( spotLight );
+   //==============================================================================================
+  //================================LUZ AMBIENTAL=================================================
+ //==============================================================================================
 
-  // ground
-  var mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(500, 500), new THREE.MeshPhongMaterial({ color: 0xDDDDDD, depthWrite: false }));
-  mesh.rotation.x = - Math.PI / 2;
-  mesh.receiveShadow = true;
-  scene.add(mesh);
+   /*Luz Ambiental => Esta luz ilumina globalmente todos los objetos de la escena por igual.
+   Esta luz no se puede usar para proyectar sombras, ya que no tiene una dirección. */
+
+  ambient = new THREE.AmbientLight( 0xffffff ); // soft white light
+  scene.add( ambient );
+
+   //==============================================================================================
+  //==================================SPOT LIGHT=================================================
+ //==============================================================================================
+
+   /*Esta luz se emite desde un único punto en una dirección, a lo largo de un cono que aumenta de tamaño a medida que se aleja de la luz.
+   Esta luz puede proyectar sombras,   SpotLightShadow */
+
+  spotLight = new THREE.SpotLight( 0xffffff,5 );
+  spotLight.position.set( 0, 200, 100 );
+  scene.add( spotLight );
+
+
+
+
+
+  //Grid
   var grid = new THREE.GridHelper(2000, 20, 0x000000, 0x000000);
   grid.material.opacity = 0.2;
   grid.material.transparent = true;
   scene.add(grid);
-  // model
-    // model
-
-
-    var ddsLoader = new DDSLoader();
-
+  
+  //Ambiente Sky
     var cubeMapTexture = new THREE.CubeTextureLoader().setPath('./Test/')
         .load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'], function (rgbmCubeMap) {
             rgbmCubeMap.encoding = THREE.RGBM16Encoding;
@@ -90,50 +116,36 @@ scene.add( spotLight );
 
             rgbmCubeMap.magFilter = THREE.LinearFilter;
             rgbmCubeMap.needsUpdate = true;
-            
-            //scene.background = rgbmCubeMap;
+            //Ocultar o adicionar a la escena
+            scene.background = rgbmCubeMap;
             pmremGenerator.dispose();
             pmremCubeUVPacker.dispose();
         });
+
+  //Exposicion 
     cubeMapTexture.exposure = 1;
 
-    var loader = new GLTFLoader();
-    loader.load('models/Roswell.glb', function (object) {
-        meshes = object.scene.children[0];
-        
-        scene.add(object.scene);
-        object.scene.children[0].position.set(1, 75, 1);
-        for (var i = 0; i < object.scene.children[0].children.length; i++) {
-            //Scale
-            object.scene.children[0].children[i].scale.set(0.1, 0.1, 0.1);
-            //Materials
-            object.scene.children[0].children[i].children[0].material.envMap = cubeMapTexture;
-            object.scene.children[0].children[i].children[0].material.metalness = 1;
-            object.scene.children[0].children[i].children[0].material.roughness = 1;
-            console.log(object.scene.children[0].children[0].children[0].material);
-            //shadow casting
-            object.scene.children[0].children[i].children[0].castShadow = true;
-            object.scene.children[0].children[i].children[0].receiveShadow = true;
-            //hide layers
-            if (!defaultLayers.includes(object.scene.children[0].children[i].name)) {
-                object.scene.children[0].children[i].visible = false;
-            }
-            if (object.scene.children[0].children[i].name.includes("Casters")) {
-                object.scene.children[0].children[i].children[0].material.metalness = 1;
-                object.scene.children[0].children[i].children[0].material.roughness = 0;
+    
+  //Geometria Cubo
 
-            }
-        }
-
-    });
+   var cubeGeo = new THREE.BoxGeometry(100,100,100);
+   var cubeMaterial = new THREE.MeshStandardMaterial ( { color: 0x1F7695 } );
+   var mesh = new THREE.Mesh( cubeGeo, cubeMaterial );
+   mesh.position.y = 70;    
+   scene.add(mesh);
+   
     
 
-
+  //Utilizar el renderizador WebGL.
     renderer = new THREE.WebGLRenderer({ canvas: artifactCanvas, antialias: true });
+  // Renderizador del tamaño de la ventana.
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true;
+  //Añadir el renderizador al elemento DOM body.
     container.appendChild(renderer.domElement);
+
+  //Controles
     controls = new OrbitControls(camera, renderer.domElement);
     controls.target.set(0, 50, 0);
     controls.update();
@@ -143,54 +155,41 @@ scene.add( spotLight );
     controls.staticMoving = true;
     controls.update();
 
+  //Intervalos de los parametros DevTools
     var gui = new GUI();
-    gui.add(params, 'hemiIrradiance', 10, 30);
-    gui.add(params, 'bulbPower', 0, 200);
-    gui.add(params, 'exposure', 0, 1);
-    gui.add(params, 'metalness', 0, 1);
-    gui.add(params, 'roughness', 0, 1);
-    gui.add(params, 'shadows');
+    gui.add(params, 'hemiIrradiance', 1, 5);
+    gui.add(params, 'exposure', 0.5, 1);
+    gui.add(params, 'directional', 0.5,5);
+    gui.add(params, 'ambient', 0,10);
+    gui.add(params, 'spotLight', 0,10);
     gui.open();
 
 
     window.addEventListener('resize', onWindowResize, false);
-}
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-function animate() {
-    requestAnimationFrame(animate);
-    var delta = clock.getDelta();
-    renderer.render(scene, camera);
-    controls.update();
-
-    render();
-}
-
-var previousShadowMap = false;
-function render() {
-    renderer.toneMappingExposure = Math.pow(params.exposure, 4.0); // to allow for very bright scenes.
-    renderer.shadowMap.enabled = params.shadows;
-    bulbLight.castShadow = params.shadows;
-    if (params.shadows !== previousShadowMap) {
-
-        previousShadowMap = params.shadows;
-    }
-    bulbLight.power = params.bulbPower;
-    bulbMat.emissiveIntensity = bulbLight.intensity / Math.pow(0.02, 2.0); // convert from intensity to irradiance at bulb surface
-    hemiLight.intensity = params.hemiIrradiance;
-    var time = Date.now() * 0.0005;
-    //bulbLight.position.y = Math.cos(time) * 0.75 + 1.25;
-    if (meshes)
-    for (var i = 0; i < meshes.children.length; i++) {
-
-        meshes.children[i].children[0].material.metalness = params.metalness;
-        meshes.children[i].children[0].material.roughness = params.roughness;
     }
 
 
-    renderer.render(scene, camera);
-}
+  //Evento ventana DevTools
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    function animate() {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+        controls.update();
+
+        render();
+    }
+
+    function render() {
+        renderer.toneMappingExposure = Math.pow(params.exposure, 4.0); // to allow for very bright scenes.
+        hemiLight.intensity = params.hemiIrradiance;
+        directional.intensity = params.directional;
+        ambient.intensity = params.ambient;
+        spotLight.intensity = params.spotLight;
+
+        renderer.render(scene, camera);
+    }
